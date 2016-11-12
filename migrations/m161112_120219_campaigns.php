366dbca040108d4978163aeb065643196931153a
ArtemMartyugov <amartyugov@gmail.com>
2016-11-12 15:35:52 +0300

    Создана таблица campaigns(кампании)

--- migrations/m161112_120219_campaigns.php
+++ migrations/m161112_120219_campaigns.php
@@ -6,14 +6,31 @@ class m161112_120219_campaigns extends Migration
 {
     public function up()
     {
-
+		$this->create_Table('campaigns', [
+			'id'=>Schema::TYPE_PK,
+			'name'=>Schema::TYPE_STRING . ' NOT NULL',
+			'description'=>Schema::TYPE_TEXT,
+			'id_creator'=>Schema::TYPE_INTEGER,
+			'name_contact'=>Schema::TYPE_STRING . ' NOT NULL',
+			'contact'=>Schema::TYPE_STRING . ' NOT NULL',
+			'date_created'=>Schema::TYPE_INTEGER . ' NOT NULL',
+			'date_end'=>Schema::TYPE_INTEGER,
+			'photo'=>Schema::TYPE_STRING,
+		]);
+			
+		$this->addForeignKey(
+			'fk_campaigns_creater', //название
+			'campaigns', //название таблицы
+			'id_creator', //Поле для связи во что тащим
+			'user' //откуда тащим
+			'id' //поле которое тащим
+			'CASCADE'
+		);					
     }
 
     public function down()
     {
-        echo "m161112_120219_campaigns cannot be reverted.\n";
-
-        return false;
+        $this->dropTable('campaigns');
     }
 
     /*
