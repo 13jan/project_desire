05f0025783cb94fc03dd54a9b4d38827aec44f82
ArtemMartyugov <amartyugov@gmail.com>
2016-11-12 17:12:35 +0300

    Миграция желания

--- migrations/m161112_135951_desire.php
+++ migrations/m161112_135951_desire.php
@@ -1,19 +1,34 @@
 <?php
 
 use yii\db\Migration;
+use yii\db\Schema;
 
 class m161112_135951_desire extends Migration
 {
     public function up()
     {
-
+		$this->createTable('desire', [
+			'id' => Schema::TYPE_PK,
+			'id_campaigns'=>Schema::TYPE_INTEGER. ' NOT NULL',
+			'name' => Schema::TYPE_STRING . ' NOT NULL',
+			'number' => Schema::TYPE_INTEGER. ' NOT NULL',
+			'comment' => Schema::TYPE_TEXT,
+			'number_solved' => Schema::TYPE_INTEGER. ' NOT NULL',
+		]);
+		
+		$this->addForeignKey(
+			'fk_desire_creater', //название
+			'desire', //название таблицы
+			'id_campaigns', //Поле для связи во что тащим
+			'campaigns', //откуда тащим
+			'id', //поле которое тащим
+			'CASCADE'
+		);	
     }
 
     public function down()
     {
-        echo "m161112_135951_desire cannot be reverted.\n";
-
-        return false;
+        $this->dropTable('desire');
     }
 
     /*
